87e80b487a8ac8b3bcb9859302c06eaf883c11e0
juicehaha <id.maksim0000@gmail.com>
2022-02-17 19:31:19 +0200

    Added TS_8_2

--- lab08/prj/ModulesUstynovych.h
+++ lab08/prj/ModulesUstynovych.h
@@ -3,4 +3,6 @@
 
 double s_calculation(double x, double y, double z);
 
+void copyright();
+
 #endif // MODULESUSTYNOVYCH_H_INCLUDED
--- lab08/prj/ModulesUstynovych/main.cpp
+++ lab08/prj/ModulesUstynovych/main.cpp
@@ -1,8 +1,21 @@
+#include <iostream>
+#include <clocale>
 #include <cmath>
+
+using namespace std;
 
 double s_calculation(double x, double y, double z)
 {
     double S;
     S =(abs(sqrt(abs(z-2*x))-x*sin(pow(x,2*z)*y)));
     return S;
+}
+
+void copyright(){
+    system ("chcp 1251 & cls");
+    cout << "  ------------------------\n"
+         << " | Ustynovych Maksym, CNTU |\n"
+         << " | @ustynovychm@gmail.com  |\n"
+         << " |  © All Rights Reserved  |\n"
+         << "  ------------------------\n";
 }
