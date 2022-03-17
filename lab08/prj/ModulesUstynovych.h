7b5b7508c62b0e8b4d87fc90162451697c665914
juicehaha <id.maksim0000@gmail.com>
2022-03-17 16:25:21 +0200

    Add modules for lab9

--- lab08/prj/ModulesUstynovych.h
+++ lab08/prj/ModulesUstynovych.h
@@ -1,8 +1,21 @@
 #ifndef MODULESUSTYNOVYCH_H_INCLUDED
 #define MODULESUSTYNOVYCH_H_INCLUDED
 
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
 double s_calculation(double x, double y, double z);
 
 void copyright();
+
+string rating (int rating);
+
+int avr_temperature(double temp[7]);
+
+int bits_number(int number);
 
 #endif // MODULESUSTYNOVYCH_H_INCLUDED
--- lab08/prj/ModulesUstynovych/main.cpp
+++ lab08/prj/ModulesUstynovych/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <clocale>
 #include <cmath>
+#include <string>
+#include <bitset>
 
 using namespace std;
 
@@ -19,3 +21,57 @@ void copyright(){
          << " |  © All Rights Reserved  |\n"
          << "  ------------------------\n";
 }
+
+string rating (int rating)
+{
+    if (rating >= 1 && rating <= 34 ){
+        return "FX (with obligatory repeated course";
+    }
+
+    if (rating >= 35 && rating <= 59){
+        return "F (with the possibility of re-assembly";
+    }
+
+    if (rating >= 60 && rating <= 66){
+        return "E (satisfies minimum criteria";
+    }
+
+    if (rating >= 67 && rating <= 74) {
+        return "D (not bad, but with significant number of defects";
+    }
+
+    if (rating >= 75 && rating <= 81){
+        return "C (overall correct fulfillment of certain significant number of errors";
+    }
+
+    if (rating >= 82 && rating <= 89){
+        return "B (above average some mistakes";
+    }
+
+    if (rating >= 90 && rating <= 100){
+        return "A (excellent performance with small number errors";
+    }
+}
+
+int avr_temperature(double temp[7])
+{
+    float sum, cel, far;
+    for (int i = 0; i < 7; i++) {
+        sum += temp[i];
+    }
+    cel = sum / 7;
+    far = 32 + 1.8 * cel;
+}
+
+
+
+int bits_number(int number)
+{
+    bitset<31> b_number{number};
+    if (b_number[12]) {
+        return 32 - b_number.count();
+    }
+    return b_number.count();
+}
+
+
